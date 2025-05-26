52c425a56e3f7127e58c0b271d64ebc0bfce6288
alvingustav <43889456+alvingustav@users.noreply.github.com>
2025-05-27 02:09:20 +0700

    Update app.py

--- src/web_app/app.py
+++ src/web_app/app.py
@@ -1,3 +1,6 @@
+import eventlet
+eventlet.monkey_patch()
+
 from flask import Flask, render_template, request, jsonify, flash, redirect, url_for
 from flask_socketio import SocketIO, emit
 import asyncio
